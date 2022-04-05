0cfe87b8e59ea5a9ec19f928ee0d8ceb9ca4a9cf
Mahsa Irankhah <m.irankhah1995@gmail.com>
2022-04-05 16:40:10 +0430

    footer added

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className='text-center m-5'>Dictionary</h1>
+        <h1 className="text-center m-5">Dictionary</h1>
       </header>
       <SearchWord />
+      <footer>
+        <p>
+          <a href="">open-source</a>project coded by Mahsa Irankhah
+        </p>
+      </footer>
     </div>
   );
 }
